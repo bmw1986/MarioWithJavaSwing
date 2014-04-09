3ef9e256e6b42403cf6c5dab3284fc566b5dbd9f
bmw1986 <freddy.oakes@me.com>
2014-04-08 20:36:40 -0400

    Struggling to get the image to change when a button is pressed.

--- Mario/src/thePackage/Background.java
+++ Mario/src/thePackage/Background.java
@@ -6,28 +6,28 @@ import java.io.*;
 import javax.swing.*;
 
 @SuppressWarnings("serial")
-public class Background extends JPanel {
+public class Background extends JPanel{
 
 	private int position = 0;
+	private JPanel myPanel;
 	private JButton startButton;
-	private Image backgroundImage;
+	private ImageIcon backgroundImage;
 	private String theImageLocation = "/Users/Freddy/Desktop/background.png";
 
 	public Background() throws IOException{
+		
 		this.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 0));
 		this.setPreferredSize(new Dimension(510, 450));
+		this.setBackground(Color.BLACK);
+		this.setFocusable(true);
 		
-		JPanel myPanel = new JPanel();
-		myPanel.setPreferredSize(new Dimension(510, 450));
+		myPanel = new JPanel();
 		myPanel.setLayout(new BoxLayout(myPanel, BoxLayout.Y_AXIS));
-	    
-	    JButton startButton = new JButton("Start");
-	    this.add(startButton);
-	}
-	
-	public void paintComponent(Graphics g) {
-		super.paintComponent(g);
+		myPanel.setPreferredSize(new Dimension(510, 450));
+		myPanel.setBackground(Color.BLACK);
+		myPanel.setFocusable(true);
 		
+	    startButton = new JButton("Start");
 		startButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -37,8 +37,16 @@ public class Background extends JPanel {
 			}
 	    });
 		
-		backgroundImage = new ImageIcon(theImageLocation).getImage();
-		g.drawImage(backgroundImage, position, 0, this);
+	    this.add(startButton);
+	}
+	
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+		Graphics2D g2d = (Graphics2D)g;
+		
+		backgroundImage = new ImageIcon(theImageLocation);
+		g2d.drawImage(backgroundImage.getImage(), -position, 0, this);
 		
 		System.out.println(position);
 	}
