13f8613b91b6898c0773e91fe4a86dbd873e8262
Freddy Oakes <freddy.oakes@me.com>
2014-04-23 18:38:58 -0400

    Update GamePlay.java
    Adde the ability for Mario to fall off of a pipe

--- Mario/src/thePackage/GamePlay.java
+++ Mario/src/thePackage/GamePlay.java
@@ -24,49 +24,79 @@ public class GamePlay extends JPanel implements KeyListener, ActionListener {
 	@SuppressWarnings("unused")
 	private ObjectsToHit bricksAndPipes;
 	private ImageIcon backgroundImage;
-	private String backgroundLocation = "C:/Users/ulab/Downloads/Mario/Folder for Ben/background.png";
-	private String marioLocation = "C:/Users/ulab/Downloads/Mario/Folder for Ben/mario2.png";
+	private String backgroundLocation = "/Users/Freddy/Desktop/background.png";
+	private String marioLocation = "/Users/Freddy/Desktop/mario2.png";
 	Controller nes = new Controller();
-	
+
 	public GamePlay() throws IOException {		
 		super();	
 		addKeyListener( this );		
 		add(nes);
-		
+
 		timer = new Timer(12, this);
-		
+
 		backgroundImage = new ImageIcon(backgroundLocation);
 		marioImage = new ImageIcon(marioLocation);
 		bricksAndPipes = new ObjectsToHit();
-		
+
 	    startButton = new JButton("Start");
 		startButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				add(backward);
+				add(jump);
+				add(forward);
+				remove(startButton);
 				X_Position = 450;	
 				Y_Position = 360;
 				refresh(); 
 			}});
 		
-		forward = new JButton(" -->");		
-		backward = new JButton("<-- ");		
-		jump = new JButton("Jump Up");
-				
-		add(startButton);
+		forward = new JButton(" -->");	
+		forward.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				boolean justDoIt = ObjectsToHit.checkHere(X_Position+12, Y_Position);
+				boolean justDoIt2 = ObjectsToHit.checkHere_Y(X_Position+16, Y_Position);
+				if (justDoIt) moveForward();
+				if (justDoIt2 == true) { moveForward(); Y_Position = 360; }
+			}});
 		
+		backward = new JButton("<-- ");	
+		backward.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				boolean justDoIt = ObjectsToHit.checkHere(X_Position-12, Y_Position);
+				boolean justDoIt2 = ObjectsToHit.checkHere_Y(X_Position-16, Y_Position);
+				if (justDoIt) moveBackward();
+				if (justDoIt2 == true) { moveBackward(); Y_Position = 360; }
+			}});
+		
+		jump = new JButton("Jump Up");
+		jump.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				boolean justDoIt = ObjectsToHit.checkHere(X_Position, 250);
+				//if (justDoIt == true){ moveUpward = true; Y_Position = 300; refresh();}
+				if (justDoIt) jump();
+//				refresh();
+			}});
+
+		add(startButton);
+
 //		setVisible(true);
 //		setSize(510, 450);		
 //		setFocusable(true);
-		
+
 //		setLayout(new FlowLayout(FlowLayout.CENTER, 0, 0));
 		setPreferredSize(new Dimension(510, 450));
 		setBackground(Color.BLACK);
 		setFocusable(true);
 		requestFocusInWindow();
 	}
-	
+
 	public void paintComponent(Graphics g) {
-		
+
 		g.drawImage(backgroundImage.getImage(), -X_Position, 0, null);
 		g.drawImage(marioImage.getImage(), 230, Y_Position, 40, 40, null);
 		System.out.println("X: " + X_Position);
@@ -74,35 +104,35 @@ public class GamePlay extends JPanel implements KeyListener, ActionListener {
 	}
 
 	public void moveForward() {
-		X_Position = X_Position + 4;
+		X_Position = X_Position + 10;
 		refresh();
 	}
-	
+
 	public void moveForwardFast() {
 		X_Position = X_Position + 10;
 		refresh();
 	}
 
 	public void moveBackward() { 
-		X_Position = X_Position - 4;		
+		X_Position = X_Position - 10;		
 		refresh();
 	}
-	
+
 	public void cheat(){
 		X_Position = 6000;
 		refresh();
 	}
-	
+
 	public void moveBackwardFast() {
 		X_Position = X_Position - 10;		
 		refresh();
 	}
-	
+
 	public void refresh() {
 		revalidate();
 		repaint();
 	}
-	
+
 
 	public void jump() {		//boolean result = ObjectsToHit.checkHere(X_Position, Y_Position);		
 //		if (moveUpward == true) {
@@ -129,7 +159,7 @@ public class GamePlay extends JPanel implements KeyListener, ActionListener {
 //				Y_Position = yInit;
 //			}			
 //		}
-		
+
 		moveUpward = true;
 		timer.start();
 	}
@@ -137,7 +167,7 @@ public class GamePlay extends JPanel implements KeyListener, ActionListener {
 	@Override
 	public void keyPressed(KeyEvent e) {
 		nes.setInput(KeyEvent.getKeyText(e.getKeyCode()).charAt(0));
-		
+
 		// Pressed Start (Y)
 		if(nes.getInput() == 'Y' && !beginGame){		
 			beginGame=true;
@@ -146,22 +176,26 @@ public class GamePlay extends JPanel implements KeyListener, ActionListener {
 			Y_Position = 360;
 			refresh(); 
 		}
-		
+
 		// Pressed R
 		if(nes.getInput() == 'R' && beginGame){
-			
+
 			if (iWannaGoFast == true) {
 				boolean justDoIt = ObjectsToHit.checkHere(X_Position+12, Y_Position);
-				if (justDoIt == true) moveForwardFast();
+				boolean justDoIt2 = ObjectsToHit.checkHere_Y(X_Position+12, Y_Position);
+				if (justDoIt && justDoIt2) { moveForwardFast(); Y_Position = 360; }
+				else if (justDoIt) moveForward(); 
 			} else {
 				boolean justDoIt = ObjectsToHit.checkHere(X_Position+12, Y_Position);
 				if (justDoIt == true) moveForward();
+				boolean justDoIt2 = ObjectsToHit.checkHere_Y(X_Position, Y_Position);
+				if (justDoIt2) { moveForward(); Y_Position = 360; }
 			}
 		}
-		
+
 		// Pressed L
 		if(nes.getInput() == 'L' && beginGame){
-		
+
 			if (iWannaGoFast == true) {
 				boolean justDoIt = ObjectsToHit.checkHere(X_Position-12, Y_Position);
 				if (justDoIt == true) moveBackwardFast();
@@ -170,22 +204,22 @@ public class GamePlay extends JPanel implements KeyListener, ActionListener {
 				if (justDoIt == true) moveBackward();
 			}
 		}
-		
+
 		// Pressed Jump (A)
 		if(nes.getInput() == 'A' && beginGame){
 			boolean justDoIt = ObjectsToHit.checkHere(X_Position, 250);
 			//if (justDoIt == true){ moveUpward = true; Y_Position = 300; refresh();}
 			if (justDoIt == true) jump();
 		}	
-		
+
 		// Pressed Fast (B)
 		if(nes.getInput() == 'B' && beginGame)
 			iWannaGoFast = true;
-		
+
 		// Pressed Select (x)
 		if(nes.getInput() == 'X')
 			cheat();
-	
+
 	}
 
 	@Override
@@ -194,19 +228,19 @@ public class GamePlay extends JPanel implements KeyListener, ActionListener {
 //			boolean justDoIt = ObjectsToHit.checkHere(X_Position, 250);
 //			if (justDoIt == true){ moveUpward = false; Y_Position = 360; refresh();}
 //		}
-		
+
 		if (nes.getInput() == 'B' && beginGame) iWannaGoFast = false;
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {	}
-	
+
 	@Override
 	public void actionPerformed(ActionEvent event) {
-	
+
 		boolean justDoIt = ObjectsToHit.checkHere(X_Position, Y_Position);
-		if (justDoIt == true) {
-			
+		if (justDoIt) {
+
 			if (moveUpward == true) {
 				if (count >= 0 && count < 20) {
 					Y_Position = Y_Position - 5;
@@ -224,6 +258,7 @@ public class GamePlay extends JPanel implements KeyListener, ActionListener {
 				} else if (count == 45) {
 					count = 0;
 					timer.stop();
+					Y_Position = 360;
 					moveUpward = false;
 				}
 			}
