2538ab1f1919cb58b5cd4435bc74c5de265676ef
Freddy Oakes <freddy.oakes@me.com>
2014-04-23 17:54:32 -0400

    Update Main.java

--- Mario/src/thePackage/Main.java
+++ Mario/src/thePackage/Main.java
@@ -1,17 +1,30 @@
 package thePackage;
 
+import java.awt.FlowLayout;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.*;
+
 import javax.swing.*;
 
-public class Main {
 
+@SuppressWarnings("serial")
+public class Main extends JFrame {
+
+	public Main() {
+		setFocusable(true);
+	}
+	
 	public static void main(String[] args) throws IOException {
 		
-		MarioGame frame = new MarioGame();
-		
+		JFrame frame = new JFrame();
+		GamePlay newGame = new GamePlay();
 		frame.setVisible(true);
-		frame.setSize(510, 450);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(newGame);
+		frame.setVisible(true);
+		frame.setSize(510, 450);		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);		
+		frame.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 0));
 		frame.setResizable(false);
 		frame.setLocationRelativeTo(null);	
 		frame.setFocusable(true);
